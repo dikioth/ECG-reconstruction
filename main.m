8d805ef9bdd8f89e31bf0bd7d546a804625aac09
dikioth <elvis@dikioth.me>
2019-12-09 15:40:44 +0100

    Add files via upload

--- main.m
+++ main.m
@@ -4,72 +4,66 @@
 % loading data (sampled at Fs = 125Hz).
 
 % Files are:
-%    - ECG_X_AVR
-%    - ECG_X_II
-%    - ECG_X_II_missing
-%    - and ECG_X_V
+%	Variable | Description         | File Name
+%   ------------------------------------------------------
+%     xT       Target signal.        ECG_X_AVR.mat          
+%     x1       Reference signal 1.   ECG_X_II.mat
+%     x2       Reference signal 2.   ECG_X_V.mat
+%     xTm      Missing part.         ECG_X_II_missing.mat
 %
 %   where X corresponds to patient number and found in folder 'ECG_X_aYZ'
-%
-% Folders are: ECG_1_a02, ECG_2_a03, ECG_3_a06, ECG_4_a07, ECG_5_a08
-%              ECG_6_a09, ECG_7_a11, ECG_8_a12
+%   Folders are: ECG_ ...
+%         [1_a02 | 2_a03 | 3_a06  | 4_a07 | 5_a08 |6_a09 | 7_a11 |8_a12]
+
 
 
 % clears
 close all; clc;
 
-
 % Starting w/ patient 2 (by teacher suggestion)
 p = 2; % Patient number
-M = 4;
-N = 20;
+M = 4; % Filter tap for x1
+N = 20; % Filter tap for x2
 
 % Loading data from directory.
-[xT, x1, x2, xTmissing] = getpatient(p);
+[xT, x1, x2, xTm] = getpatient(p);
 
 % Normalizing signals.
 xTn = xT - mean(xT);
 x1n = x1 - mean(x1);
 x2n = x2 - mean(x2);
-xTnmissing = xTmissing - mean(xTmissing);
+xTmn = xTm - mean(xTm);
 
-% Plot sinals.
-%subplots(xT, xTmissing, x1, x2);
-subplots(xTn, xTnmissing, x1n, x2n);
-
-%
-%ai = customRLS(xTn, x1n(1:size(xT)), M, 0.99);
+% Filter coefficients theta for x1 and x2.
+ai = customRLS(xTn, x1n(1:size(xT)), M, 0.99);
 bi = customRLS(xTn, x2n(1:size(xT)), N, 0.99);
 
+% Reconstructing the last 30 secs (3750 samples)
+xhat = getReconstruction(bi,x2n, 3750);
 
+% Performaance analysis
+[Q1, Q2] = getPerformance(xTm, xhat)
 
+% Plots
 
-% Reconstructing the last 30 secs
-% 30 secs corresponds to N = 3750 samples.
-
-
-xhat = zeros(1, 3750);
-counter = 1;
-
-for n = length(x2)-length(xTmissing)+1:length(x2)
-    %xhat(counter) = sum(ai'*x1n(n-M+1:n));
-    xhat(counter) = sum(bi'*x2n(n-N+1:n));
-    counter = counter + 1;
-end
-
+% Comparing reconstruction.
 figure;
 hold on;
-plot(xTnmissing);
+plot(xTmn);
 plot(xhat);
 hold off;
 
-%
-%%
+%% LOOP
 
-close all;
-hold on;
 
-plot(xTn);
-plot(-x2n);
-hold off;
-xlim([0 length(xTnmissing)]);
+
+
+
+
+
+
+
+
+
+
+
