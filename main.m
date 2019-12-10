28ea965c4b5a1a4e783bf7099da1a731aaabb057
dikioth <elvis@dikioth.me>
2019-12-10 15:59:32 +0100

    kallman filter

--- main.m
+++ main.m
@@ -21,9 +21,9 @@
 close all; clc;
 
 % Starting w/ patient 2 (by teacher suggestion)
-p = 2; % Patient number
-M = 4; % Filter tap for x1
-N = 20; % Filter tap for x2
+p = 8; % Patient number
+M = 5; % Filter tap for x1
+N = 105; % Filter tap for x2
 
 % Loading data from directory.
 [xT, x1, x2, xTm] = getpatient(p);
@@ -34,35 +34,51 @@ x1n = x1 - mean(x1);
 x2n = x2 - mean(x2);
 xTmn = xTm - mean(xTm);
 
-% Filter coefficients theta for x1 and x2.
-ai = customRLS(xTn, x1n(1:size(xT)), M, 0.99);
-bi = customRLS(xTn, x2n(1:size(xT)), N, 0.99);
+% % Filter coefficients theta for x1 and x2.
+% ci = customRLSh(xT, x1, x2, M, N);
+% ai = ci(1:M);
+% bi = ci(M+1:end);
+% 
+% 
+% % Reconstructing the last 30 secs (3750 samples)
+% xhatn1 = getReconstruction(ai,x1n, 3750);
+% xhatn2 = getReconstruction(bi,x2n, 3750);
+% 
+% xhat = xhatn1 + xhatn2 + mean(xT);
+% 
+% % Performaance analysis
+% [Q1, Q2] = getPerformance(xTm, xhat);
+% 
+% % Plots
+% 
+% % Comparing reconstruction.
+% figure;
+% hold on;
+% plot(xTm);
+% plot(xhat);
+% hold off;
+% 
+% % KALLMAN
+% close all;
 
-% Reconstructing the last 30 secs (3750 samples)
-xhat = getReconstruction(bi,x2n, 3750);
+[a,b,c] = kallman([x1n(1:length(xT)), x2n(1:length(xT))], xT);
+xRec1 = getReconstruction(a(1), x1n, 3750);
+xRec2 = getReconstruction(a(2), x2n, 3750);
+xRec = xRec1 + xRec2 + mean(xT);
+subplot(2,1,1);
+plot(xRec);
+subplot(2,1,2);
+plot(xTm);
 
-% Performaance analysis
-[Q1, Q2] = getPerformance(xTm, xhat)
 
-% Plots
-
-% Comparing reconstruction.
 figure;
-hold on;
-plot(xTmn);
-plot(xhat);
-hold off;
-
-%% LOOP
+plot([xTm(end-375:end), xRec(end-375:end)]);
 
 
+ylabel('Voltage [mV]');
+xlabel('N');
 
-
-
-
-
-
-
+legend('ECG II', 'Reconstructed ECG II');
 
 
 
